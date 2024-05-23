ee64772b462a67078692e3b42d04c85a70546bb0
Bob <toptaldev0121@gmail.com>
2024-05-22 23:04:19 -0400

    add / login - security

--- routes/login.js
+++ routes/login.js
@@ -6,10 +6,10 @@ const router = express.Router();
 
 // Get all logs route 
 router.post('/login', controller.login);
-// router.post('/getsecurity', controller.getsecurity);
-// router.post('/checksecurity', controller.checksecurity);
-// router.post('/resetpwdemail', controller.resetpwdemail);
-// router.post('/updatepwd', controller.updatepwd);
-// router.post('/updateloginouttime', controller.updateloginouttime);
-// router.post('/logout', controller.logout);
+router.post('/getsecurity', controller.getsecurity);
+router.post('/checksecurity', controller.checksecurity);
+router.post('/resetpwdemail', controller.resetpwdemail);
+router.post('/updatepwd', controller.updatepwd);
+router.post('/updateloginouttime', controller.updateloginouttime);
+router.post('/logout', controller.logout);
 module.exports = router;
